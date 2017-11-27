21f78d1fad61032800f150e97da30c7bff47b416
Amos Hason <amos@hason.science>
2017-11-27 17:40:01 +0200

    reorganization

--- django/local_gallery/archive_manager/models.py
+++
@@ -1,7 +0,0 @@
-from django.contrib.gis.db import models
-
-class Location(models.Model):
-    name = models.CharField(max_length = 100)
-    point = models.PointField()
-    information = models.TextField(blank = True)
-
