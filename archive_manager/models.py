14112cd6464a55315854e4d28e6ce1ef0a8dd7d3
Amos Hason <amos@hason.science>
2017-11-27 18:03:41 +0200

    PEP

--- archive_manager/models.py
+++ archive_manager/models.py
@@ -1,7 +1,6 @@
 from django.contrib.gis.db import models
 
 class Location(models.Model):
-    name = models.CharField(max_length = 100)
+    name = models.CharField(max_length=100)
     point = models.PointField()
-    information = models.TextField(blank = True)
-
+    information = models.TextField(blank=True)
