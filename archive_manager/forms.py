cc447260689c3bff690cb3a198b178cfe4ad45db
Shai Efrati <shaief@gmail.com>
2017-12-13 14:48:03 +0200

    Add SelectDateWidget to make adding a date easier and clearer

--- archive_manager/forms.py
+++ archive_manager/forms.py
@@ -1,3 +1,5 @@
+import datetime
+
 from django import forms
 from django.forms import ModelForm, Textarea
 from .models import Photo, Location
@@ -6,11 +8,15 @@ from .models import Photo, Location
 class PostPhoto(forms.ModelForm):
 
     class Meta:
+        def last_years():
+            first_year = datetime.datetime.now().year - 200
+            return list(range(datetime.datetime.now().year, first_year, -1))
+
         model = Photo
         widgets = {
             'content': forms.Textarea(attrs={'cols': 80, 'rows': 20}),
             'name': forms.TextInput(attrs={'class': 'form-control'}),
             'photo_file': forms.FileInput(attrs={'class': 'form-control-file'}),
-            'date_taken': forms.DateInput(attrs={'class': 'form-'}),
+            'date_taken': forms.SelectDateWidget(years=last_years(), attrs={'class': 'form-'}),
         }
         fields = ('name', 'photo_file', 'date_taken', 'location')
