b5b699eca3b11b3d96d53258e16f70cecbb34bb4
AmosHason <amos@hason.science>
2017-12-20 21:49:23 +0200

    REST API Integration - added serializers.py

---
+++ archive_manager/serializers.py
@@ -0,0 +1,19 @@
+from archive_manager.models import Location
+from drf_extra_fields.geo_fields import PointField
+from rest_framework import serializers
+
+
+class LocationSerializer(serializers.Serializer):
+    name = serializers.CharField(max_length=100)
+    point = PointField()
+    information = serializers.CharField(max_length=15)
+
+    def create(self, validated_data):
+        return Location.objects.create(**validated_data)
+    
+    def update(self, instance, validated_data):
+        instance.name = validated_data.get('name', instance.name)
+        instance.point = validated_data.get('point', instance.point)
+        instance.information = validated_data.get('information', instance.information)
+        instance.save()
+        return instance
