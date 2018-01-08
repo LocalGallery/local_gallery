8175aef62f023ca6a7f2a7d0ce4108cd8fe87e72
AmosHason <amos@hason.science>
2018-01-08 11:24:52 +0200

    Added Project model

--- archive_manager/admin.py
+++ archive_manager/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.contrib.gis.admin import OSMGeoAdmin
 
-from .models import Location, Photo
+from .models import Location, Photo, Project
 
+admin.site.register(Project, OSMGeoAdmin)
 admin.site.register(Location, OSMGeoAdmin)
 admin.site.register(Photo)
--- archive_manager/models.py
+++ archive_manager/models.py
@@ -2,9 +2,20 @@ from django import forms
 from django.contrib.gis.db import models
 from django.contrib.postgres.fields import ArrayField
 
+class Project(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    logo_file = models.ImageField('Logo',
+                                   upload_to='photos/logos')
+    center = models.PointField()
+    zoom_level = models.IntegerField()
+
+    def __str__(self):
+        return "[{}]: {}".format(self.id, self.name)
 
 class Location(models.Model):
-    name = models.CharField(max_length=100, unique=True)
+    project = models.ForeignKey('Project', on_delete=models.CASCADE,
+                                 related_name="locations")
+    name = models.CharField(max_length=100)
     point = models.PointField()
     information = models.TextField(blank=True)
 
